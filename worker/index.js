02cf1578630b74b2e47e81ddad5e338bc47f00c9
Thiago Hlebanja Oliva <thiago.hlebanja.oliva@tietoevry.com>
2021-05-27 11:13:20 +0200

    add dockerfiles.dev and docker-compose.yml ; everything runs but app doesnt work

--- worker/index.js
+++ worker/index.js
@@ -1,18 +1,17 @@
-const key = require('./keys');
-const redis = require('redis');
 const keys = require('./keys');
+const redis = require('redis');
 
 const redisClient = redis.createClient({
     host: keys.redisHost,
     port: keys.redisPort,
-    retry_strategy: () => 1000
+    retry_strategy: () => 1000,
 });
 const sub = redisClient.duplicate();
 
-function fib(index){
+function fib(index) {
     if (index < 2) return 1;
     return fib(index - 1) + fib(index - 2);
-} 
+}
 
 sub.on('message', (channel, message) => {
     redisClient.hset('values', message, fib(parseInt(message)));
